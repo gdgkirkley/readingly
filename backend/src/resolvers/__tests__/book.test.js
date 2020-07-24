25bf1076238c746923bc112829607c8fe7105ffb
gdgkirkley <gdgkirkley@gmail.com>
2020-07-23 22:21:12 -0700

    Add book query tests

--- backend/src/resolvers/__tests__/book.test.js
+++ backend/src/resolvers/__tests__/book.test.js
@@ -1,4 +1,4 @@
-import {loginUser, authRequest} from '../../../test/api'
+import api, {loginUser, authRequest} from '../../../test/api'
 import {buildBook, getDescription} from '../../../test/generate'
 
 let login
@@ -7,40 +7,88 @@ beforeEach(async () => {
   login = await loginUser('gkirkley@readingly.com', 'gkirkley')
 })
 
+test('books returns an array of books', async () => {
+  const expectedResult = {
+    id: '1',
+    title: 'Pride and Prejudice',
+    description:
+      'Austen’s most celebrated novel tells the story of Elizabeth Bennet, a bright, lively young woman with four sisters, and a mother determined to marry them to wealthy men. At a party near the Bennets’ home in the English countryside, Elizabeth meets the wealthy, proud Fitzwilliam Darcy. Elizabeth initially finds Darcy haughty and intolerable, but circumstances continue to unite the pair. Mr. Darcy finds himself captivated by Elizabeth’s wit and candor, while her reservations about his character slowly vanish. The story is as much a social critique as it is a love story, and the prose crackles with Austen’s wry wit.',
+  }
+
+  const bookData = await api.post(`${process.env.API_URL}`, {
+    query: `
+      query {
+        books {
+          id
+          title
+          description
+        }
+      }
+    `,
+  })
+
+  expect(bookData.data.books).toHaveLength(1)
+  expect(bookData.data.books[0]).toStrictEqual(expectedResult)
+})
+
+test('book returns a book', async () => {
+  const expectedResult = {
+    id: '1',
+    title: 'Pride and Prejudice',
+    description:
+      'Austen’s most celebrated novel tells the story of Elizabeth Bennet, a bright, lively young woman with four sisters, and a mother determined to marry them to wealthy men. At a party near the Bennets’ home in the English countryside, Elizabeth meets the wealthy, proud Fitzwilliam Darcy. Elizabeth initially finds Darcy haughty and intolerable, but circumstances continue to unite the pair. Mr. Darcy finds himself captivated by Elizabeth’s wit and candor, while her reservations about his character slowly vanish. The story is as much a social critique as it is a love story, and the prose crackles with Austen’s wry wit.',
+  }
+
+  const bookData = await api.post(`${process.env.API_URL}`, {
+    query: `
+      query($id: ID!) {
+        book(id: $id) {
+          id
+          title
+          description
+        }
+      }
+    `,
+    variables: {id: 1},
+  })
+
+  expect(bookData.data.book).toStrictEqual(expectedResult)
+})
+
 test('search books returns book results', async () => {
   const data = await authRequest(
     `
-                mutation($search: String!) {
-                    searchBook(search: $search) {
-                        title
-                    }
-                }
-            `,
+      mutation($search: String!) {
+          searchBook(search: $search) {
+              title
+          }
+      }
+  `,
     {search: 'Lord of the Rings'},
     login.signIn.token,
   )
 
-  expect(data.searchBook.length).toBe(10)
+  expect(data.searchBook).toHaveLength(10)
   expect(data.searchBook).toContainEqual({title: 'The Lord of the Rings'})
 })
 
 test('user can search and add book', async () => {
   const data = await authRequest(
     `
-                mutation($search: String!) {
-                    searchBook(search: $search) {
-                        title
-                        description
-                        googleBooksId
-                        authors {
-                          name
-                        }
-                        thumbnail
-                        pageCount
-                        publishDate
-                    }
+        mutation($search: String!) {
+            searchBook(search: $search) {
+                title
+                description
+                googleBooksId
+                authors {
+                  name
                 }
-            `,
+                thumbnail
+                pageCount
+                publishDate
+            }
+        }
+    `,
     {search: 'Lord of the Rings'},
     login.signIn.token,
   )
@@ -130,8 +178,6 @@ test('user can add book and update book', async () => {
     {id: bookData.createBook.id, description: newDescription},
     login.signIn.token,
   )
-
-  console.log(updateBookData)
 
   expect(updateBookData.updateBook.description).toBe(newDescription)
   expect(updateBookData.updateBook.title).toBe(book.title)
