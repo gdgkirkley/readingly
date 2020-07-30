ca10a06bc35ff1aea00ab7756fd0f72bb35163c3
gdgkirkley <gdgkirkley@gmail.com>
2020-07-29 21:43:08 -0700

    Improve AddToBookshelf test

--- frontend/components/__tests__/AddToBookshelf.test.tsx
+++ frontend/components/__tests__/AddToBookshelf.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { MockedProvider } from "@apollo/client/testing";
 import { render, cleanup, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import AddToBookshelf from "../AddToBookshelf";
 import { CURRENT_USER_QUERY } from "../../graphql/user";
 import {
@@ -17,6 +18,9 @@ test("<AddToBookshelf /> renders when user present", async () => {
   const user = await buildUser();
   const bookshelf = await buildBookshelf();
   const book = await buildBook();
+
+  let addBookMutationCalled = false;
+  const originalBookCount = bookshelf.bookCount;
 
   const mocks = [
     {
@@ -45,6 +49,38 @@ test("<AddToBookshelf /> renders when user present", async () => {
         },
       },
     },
+    {
+      request: {
+        query: ADD_BOOK_MUTATION,
+        variables: {
+          googleBookId: book.googleBooksId,
+          bookshelfId: bookshelf.id,
+        },
+      },
+      result: () => {
+        addBookMutationCalled = true;
+        bookshelf.bookCount = bookshelf.bookCount + 1;
+        bookshelf.books.push(book);
+        return {
+          data: {
+            addBook: {
+              bookCount: bookshelf.bookCount,
+            },
+          },
+        };
+      },
+    },
+    {
+      request: {
+        query: MY_BOOKSHELVES_QUERY,
+        variables: {},
+      },
+      result: {
+        data: {
+          mybookshelves: [bookshelf],
+        },
+      },
+    },
   ];
 
   render(
@@ -53,8 +89,21 @@ test("<AddToBookshelf /> renders when user present", async () => {
     </MockedProvider>
   );
 
+  let button: HTMLElement;
+
   await waitFor(() => {
-    expect(screen.getByRole("button")).toBeInTheDocument();
-    expect(screen.getByRole("button")).toHaveTextContent(/bookshelf/i);
+    button = screen.getByRole("button");
+  });
+
+  expect(button).toBeInTheDocument();
+  expect(button).toHaveTextContent(/bookshelf/i);
+
+  userEvent.click(button);
+
+  await waitFor(() => {
+    expect(addBookMutationCalled).toBeTruthy();
+    expect(button).toBeInTheDocument();
+    expect(bookshelf.books).toContain(book);
+    expect(bookshelf.bookCount).toBe(originalBookCount + 1);
   });
 });
