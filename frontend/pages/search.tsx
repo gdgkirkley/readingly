6ef71ce8f0af56a2bd92fd32b76f321140e69868
gdgkirkley <gdgkirkley@gmail.com>
2020-07-30 23:57:36 -0700

    Add pagination to search results

--- frontend/pages/search.tsx
+++ frontend/pages/search.tsx
@@ -5,15 +5,28 @@ import { useLazyQuery } from "@apollo/client";
 import { BOOK_SEARCH, BookData } from "../graphql/books";
 import SearchBar, { SearchInputs } from "../components/SearchBar";
 import BookGallery from "../components/BookGallery";
+import Button from "../components/styles/ButtonStyles";
 
 const StyledSearchPage = styled.div`
   width: ${(props) => props.theme.maxWidth};
 `;
 
+const Action = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 2rem;
+`;
+
 const SearchPage = () => {
   const router = useRouter();
   const [urlSearch, setURLSearch] = useState<string | null>(null);
-  const [searchBook, { data, loading }] = useLazyQuery<BookData>(BOOK_SEARCH);
+  const [searchBook, { data, loading, fetchMore }] = useLazyQuery<BookData>(
+    BOOK_SEARCH,
+    {
+      fetchPolicy: "cache-and-network",
+      notifyOnNetworkStatusChange: true,
+    }
+  );
 
   useEffect(() => {
     if (router.query?.q) {
@@ -42,11 +55,31 @@ const SearchPage = () => {
     });
   };
 
+  const handleLoadMore = (): void => {
+    fetchMore({
+      variables: {
+        offset: data.searchBook.length,
+      },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        if (!fetchMoreResult) return prev;
+        return Object.assign({}, prev, {
+          searchBook: [...prev.searchBook, ...fetchMoreResult.searchBook],
+        });
+      },
+    });
+  };
+
   return (
     <StyledSearchPage>
       <SearchBar handleSearch={handleSearch} defaultValue={urlSearch} />
-      {!loading && data?.searchBook ? (
-        <BookGallery books={data.searchBook} />
+      {data?.searchBook ? <BookGallery books={data.searchBook} /> : null}
+      {loading ? <p>Loading...</p> : null}
+      {data?.searchBook?.length ? (
+        <Action>
+          <Button themeColor="red" onClick={handleLoadMore} disabled={loading}>
+            Load{loading ? "ing" : ""} More
+          </Button>
+        </Action>
       ) : null}
     </StyledSearchPage>
   );
