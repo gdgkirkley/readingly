02181cf7ccad0c273778bc859b056979eb4bdf79
gdgkirkley <gdgkirkley@gmail.com>
2020-08-01 16:01:40 -0700

    Update test for BookGallery to include remove buttons

--- frontend/components/__tests__/BookGallery.test.tsx
+++ frontend/components/__tests__/BookGallery.test.tsx
@@ -1,20 +1,42 @@
 import React from "react";
 import { render, cleanup, screen, waitFor } from "@testing-library/react";
+import { MockedProvider } from "@apollo/client/testing";
 import BookGallery from "../BookGallery";
-import { buildBook } from "../../test/generate";
+import { buildBook, buildBookshelf } from "../../test/generate";
+import { Book } from "../../graphql/books";
+
+let books: Book[];
+
+beforeEach(async () => {
+  const book1 = await buildBook();
+  const book2 = await buildBook();
+  const book3 = await buildBook();
+  books = [book1, book2, book3];
+});
 
 afterEach(() => {
   cleanup();
 });
 
 test("<BookGallery /> renders a list of book cards", async () => {
-  const book1 = await buildBook();
-  const book2 = await buildBook();
-  const book3 = await buildBook();
-  const books = [book1, book2, book3];
-
   render(<BookGallery books={books} />);
 
   expect(screen.getByTestId("cards").hasChildNodes).toBeTruthy();
   expect(screen.getByTestId("cards").childNodes).toHaveLength(3);
+});
+
+test("<BookGallery /> renders a list of book cards with remove buttons", async () => {
+  const bookshelf = await buildBookshelf();
+  render(
+    <MockedProvider>
+      <BookGallery books={books} displayRemove={true} bookshelf={bookshelf} />
+    </MockedProvider>
+  );
+
+  expect(screen.getByTestId("cards")).toBeInTheDocument();
+
+  const removeButtons = screen.getAllByRole("button");
+
+  expect(removeButtons[0]).toHaveTextContent("x");
+  expect(removeButtons).toHaveLength(books.length);
 });
