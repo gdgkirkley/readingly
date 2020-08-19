bba8c8ed34b11d96374b3f9231fb8891b4af51fe
gdgkirkley <gdgkirkley@gmail.com>
2020-08-18 18:01:44 -0700

    Add the www domain to CORS

--- backend/src/cors.js
+++ backend/src/cors.js
@@ -2,7 +2,7 @@ export function corsOrigins() {
   return {
     origin:
       process.env.NODE_ENV === 'production'
-        ? ['https://readingly.ca']
+        ? ['https://readingly.ca', 'https://www.readingly.ca']
         : ['http://localhost:3000'],
     optionsSuccessStatus: 200,
     credentials: true,
