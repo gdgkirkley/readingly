49389f88c25cd30199cda3dc29ff7868d938c207
gdgkirkley <gdgkirkley@gmail.com>
2020-12-08 00:04:03 -0800

    Add icons to GoalDisplay
    This adds icons to improve the readability of the GoalDisplay component

--- frontend/components/Goal/GoalDisplay/index.tsx
+++ frontend/components/Goal/GoalDisplay/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import { GoalStatus } from "../../../graphql/goal";
 import { formatDate } from "../../../lib/formatDates";
 import {
@@ -7,63 +8,108 @@ import {
   getReadingTimeString,
   humanReadableTimeDiff,
 } from "../../../lib/time";
+import BookReader from "../../icons/BookReader";
+import GoalIcon from "../../icons/Goal";
+import { Hourglass, HourglassEnd } from "../../icons/Hourglass";
+
+const GoalStyles = styled.div`
+  align-items: flex-start;
+
+  & svg {
+    width: 3rem;
+  }
+`;
+
+const RowStyle = styled.div`
+  display: flex;
+  margin-bottom: 2.5rem;
+`;
+
+const RowText = styled.div`
+  margin-left: 2rem;
+`;
+
+const RowHeader = styled.div`
+  font-weight: 600;
+`;
 
 const GoalDisplay = ({ title, goal }) => {
   const complete = goal.status === GoalStatus.Complete;
 
   return (
-    <>
-      <p data-testid="goalDate">
-        My goal {complete ? "was" : "is"} to read {title} by{" "}
-        <strong>{formatDate(goal.goalDate)}</strong>. That{" "}
-        <span
-          dangerouslySetInnerHTML={{
-            __html: getPeriodFromNow(goal.goalDate),
-          }}
-        />
-        .
-      </p>
+    <GoalStyles>
+      <RowStyle>
+        <GoalIcon />
+        <RowText data-testid="goalDate">
+          <RowHeader>Goal</RowHeader>
+          <div>
+            My goal {complete ? "was" : "is"} to read {title} by{" "}
+            <strong>{formatDate(goal.goalDate)}</strong>. That{" "}
+            <span
+              dangerouslySetInnerHTML={{
+                __html: getPeriodFromNow(goal.goalDate),
+              }}
+            />
+            .
+          </div>
+        </RowText>
+      </RowStyle>
       {goal.status === GoalStatus.InProgress && goal.readingRecommendation ? (
-        <p data-testid="readingRecommendation">
-          To reach my goal, readingly recommends{" "}
-          <strong>reading {goal.readingRecommendation} pages today</strong>
-          {goal.readingRecommendationSeconds ? (
-            <span>
-              , which will take about{" "}
-              <strong>
-                {getReadingTimeString(goal.readingRecommendationSeconds)}
-              </strong>
-            </span>
-          ) : null}
-          !
-        </p>
+        <RowStyle>
+          <BookReader />
+          <RowText data-testid="readingRecommendation">
+            <RowHeader>Recommendation</RowHeader>
+            <div>
+              To reach my goal, readingly recommends{" "}
+              <strong>reading {goal.readingRecommendation} pages today</strong>
+              {goal.readingRecommendationSeconds ? (
+                <span>
+                  , which will take about{" "}
+                  <strong>
+                    {getReadingTimeString(goal.readingRecommendationSeconds)}
+                  </strong>
+                </span>
+              ) : null}
+              !
+            </div>
+          </RowText>
+        </RowStyle>
       ) : null}
       {goal.startDate ? (
-        <p data-testid="startDate">
-          I started reading on <strong>{formatDate(goal.startDate)}</strong>.
-          That{" "}
-          <span
-            dangerouslySetInnerHTML={{
-              __html: getPeriodFromNow(goal.startDate),
-            }}
-          />
-          .{" "}
-        </p>
+        <RowStyle>
+          <Hourglass />
+          <RowText data-testid="startDate">
+            <RowHeader>Start Date</RowHeader>I started reading on{" "}
+            <strong>{formatDate(goal.startDate)}</strong>. That{" "}
+            <span
+              dangerouslySetInnerHTML={{
+                __html: getPeriodFromNow(goal.startDate),
+              }}
+            />
+            .{" "}
+          </RowText>
+        </RowStyle>
       ) : null}
       {goal.endDate ? (
-        <p>
-          I finished reading on <strong>{formatDate(goal.endDate)}</strong>. It
-          took me{" "}
-          <strong>
-            {humanReadableTimeDiff(
-              new Date(goal.startDate),
-              new Date(goal.endDate)
-            )}
-          </strong>{" "}
-          to read.
-        </p>
+        <RowStyle>
+          <HourglassEnd />
+          <RowText>
+            <RowHeader>End Date</RowHeader>
+            <div>
+              I finished reading on <strong>{formatDate(goal.endDate)}</strong>.
+              It took me{" "}
+              <strong>
+                {humanReadableTimeDiff(
+                  new Date(goal.startDate),
+                  new Date(goal.endDate)
+                )}
+              </strong>{" "}
+              to read.
+            </div>
+          </RowText>
+        </RowStyle>
       ) : null}
-    </>
+    </GoalStyles>
   );
 };
 
